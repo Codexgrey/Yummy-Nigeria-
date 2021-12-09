04327899fbcf776079bdb4f8a4d5e94c43424ddd
Codexgrey <thecodexgrey@gmail.com>
2021-12-09 13:44:51 +0100

    added tags feature

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
 from taggit.models import Tag
-from .models import Post#, Comment
+from .models import Post, Comment
 from django.db.models import Count
 from django.core.mail import send_mail
 from django.views.generic import ListView
-from .forms import EmailPostForm#, CommentForm
+from .forms import EmailPostForm, CommentForm
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -34,15 +34,42 @@ def post_list(request, tag_slug=None):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
-        Post, 
-        slug=post, 
-        status='published', 
-        publish__year=year, 
-        publish__month=month, 
+        Post,
+        slug=post,
+        status='published',
+        publish__year=year,
+        publish__month=month,
         publish__day=day
     )
 
-    context = {'post': post}
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    post_tags_ids = post.tag.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tag__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tag')).order_by('-same_tags','-publish')[:5]
+
+    context = {
+        'post': post, 
+        'comments': comments, 
+        'new_comment': new_comment, 
+        'comment_form': comment_form,
+        'similar_posts': similar_posts
+    }
     return render(request, 'recipes/post/detail.html', context)
 
 
